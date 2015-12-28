8a52b1e9a6bedea508b0907809b5695ce0999e2d
Daniel Warner <danywarner@gmail.com>
2015-12-28 18:31:37 -0500

    overlay buttons added

--- swipeswift/OverlayView.swift
+++ swipeswift/OverlayView.swift
@@ -16,7 +16,7 @@ class OverlayView: UIView {
     }
 
     private var _mode: CGOverlayViewMode!
-    
+    private var imageView: UIImageView!
     var mode: CGOverlayViewMode {
         get {
             return _mode
@@ -24,15 +24,24 @@ class OverlayView: UIView {
         
         set {
             _mode = newValue
+            if _mode == CGOverlayViewMode.GGOverlayViewModeLeft {
+                imageView.image = UIImage(named: "noButton")
+            } else {
+                imageView.image = UIImage(named: "yesButton")
+            }
         }
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = CGRectMake(50, 50, 100, 100);
+    }
     
     
     override init(frame: CGRect) {
-        _mode = CGOverlayViewMode.GGOverlayViewModeLeft
         super.init(frame: frame)
-        
+        imageView = UIImageView(image: UIImage(named: "noButton"))
+        self.addSubview(imageView)
     }
 
     required init?(coder aDecoder: NSCoder) {
